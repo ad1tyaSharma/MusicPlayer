fa0c4ce84007dfe758c1e1fc04cc6fce4bf0e81d
Aditya-011 <adihgr18@gmail.com>
2021-05-24 18:21:46 +0530

    UI Tweaks Part 1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index2.ejs");
 });
 
 module.exports = router;
